9d377df09637e286cb326fce071bb69d5ea902e1
Jérôme Fink <jerome.fink@unamur.be>
2023-09-14 16:06:51 +0200

    Creating a training class to track experiments

---
+++ training/classification_trainer.py
@@ -0,0 +1,109 @@
+from typing import NewType, TypeVar, Literal
+
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import datetime
+
+DataLoaders = NewType("DataLoaders", dict[Literal["train", "test"], DataLoader])
+Model = TypeVar("Model", bound=torch.nn.Module)
+Optimizer = TypeVar("Optimizer", bound=torch.optim.Optimizer)
+Criterion = TypeVar("Criterion", bound=torch.nn.Module)
+
+
+class ClassificationTrainer:
+    def __init__(
+        self,
+        model: Model,
+        optimizer: Optimizer,
+        criterion: Criterion,
+        scheduler=None,
+        device: torch.device = None,
+        verbose: bool = True,
+        gradient_clip=False,
+    ):
+        self.model = model
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.scheduler = scheduler
+
+        if device is None:
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.device = device
+        self.verbose = verbose
+        self.gradient_clip = gradient_clip
+
+        self.train_metrics = []
+        self.test_metrics = []
+
+        self.current_epoch = 0
+
+    def add_train_metric(self, metric):
+        self.train_metrics.append(metric)
+
+    def add_test_metric(self, metric):
+        self.test_metrics.append(metric)
+
+    def make_prediction(self, features, targets):
+        logits = self.model(features)
+        prediction = torch.max(logits, -1)[1]
+
+        loss = self.criterion(logits, targets)
+
+        return logits, prediction, loss
+
+    def train_epoch(self):
+        self.model.train()
+
+        progress_bar = tqdm(
+            self.data["train"], desc="Training", disable=(not self.verbose)
+        )
+
+        for index, (features, target) in enumerate(progress_bar):
+            features = features.to(self.device)
+            target = target.to(self.device)
+
+            logits, prediction, loss = self.make_prediction(features, target)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+
+            if self.gradient_clip:
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1)
+
+            self.optimizer.step()
+
+    def test_epoch(self):
+        self.model.eval()
+
+        progress_bar = tqdm(
+            self.data["test"], desc="Testing", disable=(not self.verbose)
+        )
+
+        for index, (features, target) in enumerate(progress_bar):
+            features = features.to(self.device)
+            target = target.to(self.device)
+
+            logits, prediction, loss = self.make_prediction(features, target)
+
+    def fit(self, epochs: int, save_best: bool = False):
+        start = datetime.now()
+        self.model.to(self.device)
+        best_metric = None
+
+        for epoch in range(self.current_epoch, epochs):
+            self.current_epoch = epoch
+
+            print("-" * 10, "EPOCH", epoch + 1, "/", epochs)
+            self.train_epoch()
+            self.test_epoch()
+
+            if self.scheduler is not None:
+                self.scheduler.step()
+
+            # SAVE EXPERIMENT AND MODEL
+
+        delta = datetime.now() - start
+        print("-" * 10)
+        print(f"TRAINING COMPLETED [{delta}].")
