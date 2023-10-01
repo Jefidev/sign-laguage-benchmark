e8293fe05e5b31347daf05476bdcc918be39aee9
Jérôme Fink <jerome.fink@unamur.be>
2023-10-01 23:21:10 +0200

    Changing slur namr

--- slurms/contrastive_250.sh
+++ slurms/contrastive_250.sh
@@ -20,9 +20,9 @@ module purge
 module load PyTorch
 
 source ./venv/bin/activate
-# pip install --ignore-installed -r requirements.txt
+pip install -r requirements.txt
 nvidia-smi
 python VIT_contrastive.py \
  -l 250\
- -e contrastive-test-lucia\
+ -e contrastive-250\
  -d /gpfs/projects/acad/lsfb/datasets/lsfb_v2/isol \
